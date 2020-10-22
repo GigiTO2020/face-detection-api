af960f09262114263abbc24dc60ddd395c6d6d60
GigiTO2020 <66763292+GigiTO2020@users.noreply.github.com>
2020-10-22 19:02:44 -0400

    Uploading server

---
+++ controllers/image.js
@@ -0,0 +1,30 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: '0e7fcf1d9bb341eeb2379e032e095bb2'
+});
+
+const handleApiCall = (req, res) => {
+    app.models
+        .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => res.status(400).json('unable to work with API'))
+}
+
+
+const handleImage = (req, res, db) => {
+    const { id } = req.body;
+    db('users').where('id', '=', id)
+        .increment('entries', 1)
+        .returning('entries')
+        .then(entries => {
+            res.json(entries[0]);
+        })
+        .catch(err => res.status(400).json('unable to get entries'))
+}
+module.exports = {
+    handleImage,
+    handleApiCall
+}
