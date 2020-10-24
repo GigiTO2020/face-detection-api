7ca5281ac2e339dbe555470377c5ecde6fdce7fa
GigiTO2020 <66763292+GigiTO2020@users.noreply.github.com>
2020-10-24 10:49:32 -0400

    update 1.0

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,4 +1,4 @@
-const handleSignin = (req, res, db, bcrypt) => {
+const handleSignin = (db, bcrypt) => (req, res) => {
     const { email, password } = req.body;
     if (!email || !password) {
         return res.status(400).json('incorrect form submission');
@@ -6,7 +6,7 @@ const handleSignin = (req, res, db, bcrypt) => {
     db.select('email', 'hash').from('login')
         .where('email', '=', email)
         .then(data => {
-            const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+            const isValid = bcrypt.compareSync(password, data[0].hash);
             if (isValid) {
                 return db.select('*').from('users')
                     .where('email', '=', email)
@@ -20,6 +20,7 @@ const handleSignin = (req, res, db, bcrypt) => {
         })
         .catch(err => res.status(400).json('wrong credentials'))
 }
+
 module.exports = {
     handleSignin: handleSignin
-};
+}
